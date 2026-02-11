767c5e4861c0a5c1ae75576ee61fa00a8b776bde
Musiki8213 <sithomolamusiki@gmail.com>
2026-02-11 15:23:38 +0200

    added the restaurant project

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,7 +46,7 @@ export const Hero = () => {
                   <svg className="w-6 h-6 md:w-7 md:h-7 animate-pulse-slow" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                   </svg>
-                  UI/UX Full Stack Developer
+                   Full Stack Developer
                 </span>
               </p>
             </div>
